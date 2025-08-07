4980f49c9e6f0808e65f05519c764256ccb9145b
bloodofspring <t.shagin@gmail.com>
2025-08-07 17:12:05 +0300

    feat: Added password-asking page and session creation

--- app/actions/startCmd.go
+++ app/actions/startCmd.go
@@ -1,25 +1,96 @@
 package actions
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"main/controllers"
+	"main/database"
+	"main/database/models"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 type MainPage struct {
 	Name   string
 	Client tgbotapi.BotAPI
 }
 
-func (e MainPage) fabricateAnswer(update tgbotapi.Update) tgbotapi.MessageConfig {
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-	msg.Text = "Hi :)"
+func (m MainPage) GetSession(update tgbotapi.Update) (*models.Sessions, error) {
+	session := &models.Sessions{}
+	err := database.GetDB().Model(session).Where("user_id = ?", update.Message.From.ID).Select()
 
-	return msg
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
 }
 
-func (e MainPage) Run(update tgbotapi.Update) error {
-	if _, err := e.Client.Send(e.fabricateAnswer(update)); err != nil {
+func (m MainPage) AskPassword(update tgbotapi.Update) error {
+	response := tgbotapi.NewMessage(update.Message.Chat.ID, "Введите пароль или отправьте реплай на сообщение, текст которого содержит пароль:")
+	_, err := m.Client.Send(response)
+	if err != nil {
+		return err
+	}
+
+	stepKey := controllers.NextStepKey{
+		ChatID: update.Message.Chat.ID,
+		UserID: update.Message.From.ID,
+	}
+	stepAction := controllers.NextStepAction{
+		Func:        HandlePassword,
+		Params:      make(map[string]any),
+		CreatedAtTS: time.Now().Unix(),
+	}
+
+	controllers.GetNextStepManager().RegisterNextStepAction(stepKey, stepAction)
+
+	return nil
+}
+
+func HandlePassword(client tgbotapi.BotAPI, stepUpdate tgbotapi.Update, stepParams map[string]any) error {
+	if stepUpdate.Message.ReplyToMessage != nil {
+		reply := stepUpdate.Message.ReplyToMessage
+		stepParams["password"] = reply.Text
+	} else {
+		stepParams["password"] = stepUpdate.Message.Text
+	}
+
+	newSession := &models.Sessions{
+		UserID:    stepUpdate.Message.From.ID,
+		Password:  stepParams["password"].(string),
+	}
+
+	_, err := database.GetDB().Model(newSession).Insert()
+	if err != nil {
+		return err
+	}
+
+	return MainPage{Name: "main-page-from-step-func", Client: client}.MainPage(stepUpdate, newSession)
+}
+
+func (m MainPage) MainPage(update tgbotapi.Update, session *models.Sessions) error {
+	response := tgbotapi.NewMessage(update.Message.Chat.ID, "Главная страница")
+	_, err := m.Client.Send(response)
+	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (m MainPage) main(update tgbotapi.Update) error {
+	session, err := m.GetSession(update)
+	if err != nil {
+		return m.AskPassword(update)
+	}
+
+	return m.MainPage(update, session)
+}
+
+func (m MainPage) Run(update tgbotapi.Update) error {
+	err := m.main(update)
+
+	return err
 }
 
 func (e MainPage) GetName() string {
